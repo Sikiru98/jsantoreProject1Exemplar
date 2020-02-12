6dea4e5056cbd60620d14422eeb806a684d25fdb
sikiru <sikiruonifadejr@gmail.com>
2020-02-12 06:53:28 -0500

    done

--- CapstonePrep.py
+++ CapstonePrep.py
@@ -2,7 +2,7 @@ import requests
 import time
 from typing import Dict, List
 import sqlite3
-
+from typing import Tuple
 
 def get_github_jobs_data() -> List[Dict]:
     """retrieve github jobs data in form of a list of dictionaries after json processing"""
@@ -29,15 +29,50 @@ def save_data(data, filename='data.txt'):
             print(item, file=file)
 
 
-def save_to_db(data):
+def save_to_db(jobs, cursor, connection):
     """:keyword data is a list of dictionaries. Each dictionary is a JSON object with a bit of jobs data"""
-    pass
+    for data in jobs:
+        cursor.execute(f'''INSERT INTO jobs VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', [
+            data['id'],
+            data['type'],
+            data['url'],
+            data['created_at'],
+            data['company'],
+            data['company_url'],
+            data['location'],
+            data['title'],
+            data['description'],
+            data['how_to_apply'],
+            data['company_logo']
+            ])
+
+    connection.commit()
 
 
 def main():
     data = get_github_jobs_data()
     save_data(data)
+    filename = 'demo_db.sqlite'
+    connection, cursor = open_db(filename)
+    setup_db(cursor, connection)
+    save_to_db(data, cursor, connection)
+    close_db(connection)
 
 
+def open_db(filename:str) ->Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)  # connect to existing DB or create new one
+    cursor = db_connection.cursor()  # get ready to read/write data
+    return db_connection,cursor
+
+
+def close_db(connection:sqlite3.Connection):
+    connection.commit()#make sure any changes get saved
+    connection.close()
+
+
+def setup_db(cursor:sqlite3.Cursor, connection):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs( id TEXT PRIMARY KEY, Position_Type TEXT NOT NULL,url TEXT NOT NULL,created_at TEXT NOT NULL, company TEXT NOT NULL , company_url TEXT, Location TEXT NOT NULL, Title TEXT NOT NULL, Description TEXT NOT NULL, How_to_Apply TEXT NOT NULL, Company_Logo TEXT);''')
+    connection.commit()
+    
 if __name__ == '__main__':
     main()
--- tests/test_datagrab.py
+++ tests/test_datagrab.py
@@ -14,6 +14,35 @@ def test_jobs_dict(get_data):
     assert type(get_data[1]) is dict
 
 
+def test_connectdb():
+    data = CapstonePrep.get_github_jobs_data()
+    filename = 'demo_db.sqlite'
+    connection,cursor = CapstonePrep.open_db( filename )
+    CapstonePrep.setup_db( cursor,connection )
+    CapstonePrep.save_to_db( data,cursor,connection )
+    CapstonePrep.close_db( connection )
+
+    connection,cursor = CapstonePrep.open_db( filename )
+    cursor.execute("SELECT * FROM jobs where jobs.title = 'Account Executive'")
+    assert cursor.fetchone()
+
+
+def test_connectdb1():
+    data = CapstonePrep.get_github_jobs_data()
+    filename = 'demo_db.sqlite'
+    connection,cursor = CapstonePrep.open_db( filename )
+    CapstonePrep.setup_db( cursor,connection )
+    gt = [{'id':"5", 'type':"sax", 'url': "x",
+    'created_at': "sax",'company': "sax",'company_url': "sax",'location': "sax",
+    'title': "sax",'description': "sax",'how_to_apply': "sax", 'company_logo': "sax"}]
+    CapstonePrep.save_to_db(gt, cursor,  connection)
+    cursor.execute("SELECT * FROM jobs where jobs.title = 'sax'")
+    assert cursor.fetchone()
+
+
+
+
+
 def test_jobs_data(get_data):
     # any real data should have both full time and Contract
     # jobs in the list, assert this
